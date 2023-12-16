521d158f27ae6bd325fa62e9d9391f47b198f318
Naveen Jose <naveen.chakiath@gmail.com>
2023-12-16 19:11:48 +0530

    feat: add view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 const config = {
   site: 'https://codeandcloud.netlify.app',
+  integrations: [preact()],
 };
 
 // https://astro.build/config
